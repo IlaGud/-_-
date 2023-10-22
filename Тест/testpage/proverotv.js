8133d4638998e52e479f149b32d28235a713f769
IlaGud <134745017+IlaGud@users.noreply.github.com>
2023-10-22 10:34:16 +0300

    Add files via upload

---
+++ Тест/testpage/proverotv.js
@@ -0,0 +1,10 @@
+function changeColor() {
+    var myObject = document.getElementById("myObject");
+    myObject.style.backgroundColor = "red";
+    myObject.style.opacity = "0.5";
+  }
+  function changeColor1() {
+    var myObject = document.getElementById("myObject1");
+    myObject.style.backgroundColor = "red";
+    myObject.style.opacity = "0.5";
+  }
